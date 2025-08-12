60e51dcba9e57a4d9569b32f481dd998400a7114
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-12 22:24:42 +0530

    Update and rename config.js to firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,18 @@
-// Firebase v9 CDN SDKs
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyATZi32dliIzjOCIXcv1MbxOXhkNjogA6Q",
   authDomain: "aventattendance.firebaseapp.com",
+  databaseURL: "https://aventattendance-default-rtdb.firebaseio.com/",
   projectId: "aventattendance",
-  storageBucket: "aventattendance.firebasestorage.app",
+  storageBucket: "aventattendance.appspot.com",
   messagingSenderId: "993022737242",
   appId: "1:993022737242:web:92c380107f73eb70ac1163"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
