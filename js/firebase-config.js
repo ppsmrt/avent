5474487d19578d0fad5c932d45d32eead5a4414d
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-13 00:12:41 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,22 @@
-// Firebase v9 CDN SDK
+// Import Firebase v9 modular SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
+// Correct Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyATZi32dliIzjOCIXcv1MbxOXhkNjogA6Q",
   authDomain: "aventattendance.firebaseapp.com",
-  databaseURL: "https://aventattendance-default-rtdb.firebaseio.com",
+  databaseURL: "https://aventattendance-default-rtdb.firebaseio.com", 
   projectId: "aventattendance",
   storageBucket: "aventattendance.appspot.com",
   messagingSenderId: "993022737242",
   appId: "1:993022737242:web:92c380107f73eb70ac1163"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export for use in other files
 export const auth = getAuth(app);
 export const db = getDatabase(app);
